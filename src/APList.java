43c93c5b89141e2a6090e2d29981dd92850b257b
achang302 <alliechang8@gmail.com>
2023-11-17 14:50:08 -0800

    added safety checks, made methods more concise, added set() method

--- src/APList.java
+++ src/APList.java
@@ -1,6 +1,6 @@
 public class APList {
     private String[] list;
-    private static int DEFAULT_SIZE = 10;
+    private static final int DEFAULT_SIZE = 2;
     private int maxCapacity;
     private int lastElement;
 
@@ -19,13 +19,8 @@ public class APList {
         if (lastElement + 1 < maxCapacity) {
             list[lastElement + 1] = element;
         } else {
-            String[] newList = new String[maxCapacity * 2];
-            maxCapacity = maxCapacity * 2;
-            for (int i = 0; i < list.length; i++) {
-                newList[i] = list[i];
-            }
-            newList[lastElement + 1] = element;
-            list = newList;
+            expand();
+            add(element);
         }
         lastElement++;
     }
@@ -36,13 +31,10 @@ public class APList {
         } else if (index > maxCapacity || index < 0) {
             System.out.println("ERROR");
         } else {
-            String temp = list[index];
-            list[index] = element;
-            for (int i = index + 1; i < list.length; i++) {
-                String temp2 = list[i];
-                list[i] = temp;
-                temp = temp2;
+            for (int i = lastElement + 1; i > index; i++) {
+                list[i] = list[i - 1];
             }
+            list[index] = element;
         }
     }
 
@@ -81,22 +73,26 @@ public class APList {
 
     public int removeAll(String element) {
         int numRemoved = 0;
-        while (contains(element)) {
-            remove(element);
+        while (remove(element)) {
             numRemoved++;
         }
         return numRemoved;
     }
 
-    public int size() {
-        return lastElement;
+    public void set(int index, String element) {
+        if (index <= lastElement && index > 0) {
+            list[index] = element;
+        }
     }
 
     public String get(int index) {
         if (index >= 0 && index <= lastElement) {
             return list[index];
+        } else if (Math.abs(index) <= lastElement) {
+            return list[lastElement + 1 + index];
+        } else {
+            return null;
         }
-        return "";
     }
 
     public boolean contains(String value) {
@@ -106,5 +102,16 @@ public class APList {
             }
         }
         return false;
+    }
+
+    public int size() {
+        return lastElement;
+    }
+
+    public void expand() {
+        String[] newList = new String[maxCapacity * 2];
+        maxCapacity = maxCapacity * 2;
+        System.arraycopy(list, 0, newList, 0, list.length);
+        list = newList;
     }
 }
